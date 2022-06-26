9849bb2959a0ae56909ba0489c84326ba42e4bea
Jeremy Silliman <jsilliman@protonmail.com>
2022-06-26 09:56:38 -0400

    account for multiple cardsets in one file + newlines

--- convert.py
+++ convert.py
@@ -12,20 +12,56 @@ conn = sqlite3.connect("rectangle-dispenser.db")
 cursor = conn.cursor()
 
 with open(sys.argv[1]) as old_data:  # Play Inscryption
-    cursor.execute(f'CREATE TABLE {sys.argv[2]} {("trigger", "image", "response")}')
+    try:
+        cursor.execute(f'CREATE TABLE {sys.argv[2]} {("trigger", "image", "response")}')
+    except sqlite3.OperationalError:
+        pass
+    old_data = old_data.readlines()
+    data = []
+    for i in range(len(old_data)):
+        line = old_data[i].strip().split("~")
+        if line[0] == "Key Terms":
+            sys.argv[
+                2
+            ] = "game_help"  # ugly hack because response.txt has game help *and* official in one file
+            try:
+                cursor.execute(
+                    f'CREATE TABLE {sys.argv[2]} {("trigger", "image", "response")}'
+                )
+            except sqlite3.OperationalError:
+                pass
+        if len(line) > 1:
+            trigger = line[0]
+            image = None
+            response = line[1]
+            if "https" in response and "png" in response:
+                image = "https://" + response.split("https://")[1].rstrip()
+                response = response.split("https://")[0].rstrip()
+            data.append(
+                {
+                    "trigger": trigger,
+                    "image": image,
+                    "response": response,
+                    "cardset": sys.argv[2],
+                }
+            )
+        elif line[0]:
+            # if there's stuff in the previous line, append to the last value of data
+            prev_line = old_data[i - 1].strip().split("~")
+            if prev_line[0] and i > 2:
+                data[len(data) - 1]["response"] = (
+                    data[len(data) - 1]["response"] + f"\n{line[0]}"
+                )
 
-    for line in old_data.readlines():
-        line = line.strip().split("~")
-        trigger = line[0]
-        image = None
-        response = line[1]
-        if "https" in response and "png" in response:
-            image = "https://" + response.split("https://")[1].rstrip()
-            response = response.split("https://")[0].rstrip()
-
-        print(f"Inserting trigger {trigger}, image {image} and response {response}")
-        cursor.execute(f'DELETE FROM {sys.argv[2]} WHERE TRIGGER = "{trigger}"')
-        cursor.execute(
-            f"INSERT INTO {sys.argv[2]} VALUES (?,?,?)", (trigger, image, response)
+    for entry in data:
+        print(
+            f"Inserting trigger {entry['trigger']}, image {entry['image']} and response {entry['response']} into the {entry['cardset']} cardset"
         )
-    conn.commit()
+        cursor.execute(
+            f"DELETE FROM {entry['cardset']} WHERE TRIGGER = \"{entry['trigger']}\""
+        )
+        cursor.execute(
+            f"INSERT INTO {entry['cardset']} VALUES (?,?,?)",
+            (entry["trigger"], entry["image"], entry["response"]),
+        )
+        conn.commit()
