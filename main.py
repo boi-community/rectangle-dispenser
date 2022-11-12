6884e3fed3a0af60ed1178a8fa6c83ad4f4f889c
jerbmega <jsilliman@protonmail.com>
2022-11-11 22:59:56 -0500

    hikari 2.0.0.dev112 compatibility fix
    Message content is now a privileged intent and must explicitly be called.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ with open("config.yaml") as cfg:
     config = yaml.safe_load(cfg)
 
 if __name__ == "__main__":
-    bot = lightbulb.BotApp(token=config["token"])
+    bot = lightbulb.BotApp(
+        token=config["token"],
+        intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT,
+    )
     bot.load_extensions_from("plugins")
     bot.run()
